da49eeb4c77c03166e27cff88d136a1dfa8fdbba
MarcoXin <591764955@qq.com>
2020-07-08 11:42:09 +0800

    打包成app

--- other/无界科学上网.js
+++
@@ -1,9 +0,0 @@
-auto.waitFor();
-var Maid = require("../common/Maid.js");
-var maid = new Maid("us.ultrasurf.mobile.ultrasurf");
-maid.before(true);
-
-maid.launchActivity("us.ultrasurf.mobile.ultrasurf.MainActivity");
-maid.sleep(0.2);
-maid.clickIdCenter("mySwitch");
-maid.after();
